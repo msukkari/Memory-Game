162d8a59369ff7343443fedd1af81185e7fc13a0
msukkarieh <michael.sukkarieh@mail.mcgill.ca>
2016-11-13 23:10:29 -0500

    added memorycard and script

---
+++ Assets/Scripts/MemoryCard.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MemoryCard : MonoBehaviour {
+	[SerializeField] private GameObject cardback;
+
+	public void OnMouseDown(){
+		if (cardback.activeSelf)
+			cardback.SetActive (false);
+	}
+}
